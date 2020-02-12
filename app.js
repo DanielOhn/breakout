8618bdee46e0a4b8ddac856099205f330cbe9635
Daniel Ohn <ohndaniel@gmail.com>
2020-02-12 00:37:46 -0700

    Add keyboard and collision function>'

--- app.js
+++ app.js
@@ -130,3 +130,89 @@ function setup() {
 
 function game(delta) {
 }
+
+function check_collid(r1, r2) {
+  // Define variables we'll use to calculate
+  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  // hit will determine whether there's a collision
+  hit = false;
+
+  // Find the center points of each sprite
+  r1.centerX = r1.x;
+  r1.centerY = r1.y;
+
+  r2.centerX = r2.x;
+  r2.centerY = r2.y;
+
+  // Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  // Calculate the distance vectors between sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  // Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  // Check collision on x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+    // A collisoin might be occuring.  Check for it on y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+      // There's definitely a collision happening
+      hit = true;
+    } else {
+      hit = false;
+    }
+  } else {
+    hit = false;
+  }
+
+  return hit;
+}
+
+function keyboard(value) {
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+
+  key.downHandler = event => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
+
+  key.upHandler = event => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
+
+  // Attach Event listeners
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
+
+  window.addEventListener("keydown", downListener, false);
+  window.addEventListener("keyup", upListener, false);
+
+  // Detach event listeners
+  key.unsubscribe = () => {
+    window.removeEventListener("keydown", downListener);
+    window.removeEventListener("keyup", upListener);
+  };
+
+  return key;
+};
