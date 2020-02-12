74e10d9f102ed2858acaedb19906d1d72ae7992b
Daniel Ohn <ohndaniel@gmail.com>
2020-02-11 22:43:28 -0700

    Update index, css, and app.js.

---
+++ app.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('mycanvas');
+
+let type = 'WebGL';
+PIXI.setting.SPRITE_MAX_TEXTURES = Math.min(PIXI.settings.SPRITE_MAX_TEXTURES, 16);
+
+if (!PIXI.utils.isWebGLSupported()) {
+  type='canvas';
+}
+
+let _w = window.innerWidth;
+let _h = window.innerHeight;
+
+const app = new PIXI.Application({
+  view: canvas,
+  width: _w,
+  height: _h,
+  resolution: window.devicePixelRatio,
+  autoDensity: true
+});
+
+window.addEventListener('resize', resize);
+
+function resize() {
+  _w = window.innerWidth;
+  _h = window.innerHeight;
+
+  app.renderer.resize(_w, _h);
+}
+
